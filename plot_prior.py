b3a6cb8f7a1ebed1c28b466fc4923ed30a05e90f
Ian Czekala <iancze@gmail.com>
2019-03-16 12:55:31 -0700

    added conversions.

--- plot_prior.py
+++ plot_prior.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.integrate import quad
 
+deg = np.pi / 180.0
 
 def p(u, mu, tau):
     v = np.log(u / (1 - u))
@@ -30,3 +32,52 @@ ax.set_xlim(0, 180)
 ax.set_ylim(0, 1.02)
 fig.subplots_adjust(bottom=0.25, top=0.98, left=0.09, right=0.91)
 fig.savefig("prior.pdf")
+fig.savefig("prior.png", dpi=600)
+
+
+
+# let's also plot some blank figures and the isotropic and low-prior for kicks
+fig, ax = plt.subplots(nrows=1, figsize=(3.5, 1.8))
+ax.set_xlabel(r"$\theta\; [{}^\circ]$")
+ax.set_ylabel(r"$p(\theta)$")
+ax.yaxis.set_ticklabels([])
+ax.set_xlim(0, 180)
+ax.set_ylim(0, 1.02)
+fig.subplots_adjust(bottom=0.25, top=0.98, left=0.09, right=0.91)
+fig.savefig("prior_blank.pdf")
+fig.savefig("prior_blank.png", dpi=600)
+
+
+# let's also plot some blank figures and the isotropic and low-prior for kicks
+fig, ax = plt.subplots(nrows=1, figsize=(3.5, 1.8))
+
+dist = np.sin(us * np.pi)/2 * deg
+dist = dist / np.max(dist)
+
+ax.plot(us * 180.0, dist, lw=2, color="C0") #, alpha=0.7)
+
+ax.set_xlabel(r"$\theta\; [{}^\circ]$")
+ax.set_ylabel(r"$p(\theta)$")
+ax.yaxis.set_ticklabels([])
+ax.set_xlim(0, 180)
+ax.set_ylim(0, 1.02)
+fig.subplots_adjust(bottom=0.25, top=0.98, left=0.09, right=0.91)
+fig.savefig("prior_sin.pdf")
+fig.savefig("prior_sin.png", dpi=600)
+
+
+
+fig, ax = plt.subplots(nrows=1, figsize=(3.5, 1.8))
+
+dist = p(us, -3.0, 3.5)
+dist = dist / np.max(dist)
+ax.plot(us * 180.0, dist, lw=2, color="C0") #, alpha=0.7)
+
+ax.set_xlabel(r"$\theta\; [{}^\circ]$")
+ax.set_ylabel(r"$p(\theta)$")
+ax.yaxis.set_ticklabels([])
+ax.set_xlim(0, 180)
+ax.set_ylim(0, 1.02)
+fig.subplots_adjust(bottom=0.25, top=0.98, left=0.09, right=0.91)
+fig.savefig("prior_low.pdf")
+fig.savefig("prior_low.png", dpi=600)
